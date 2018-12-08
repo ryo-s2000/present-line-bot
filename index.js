7d75f9782f89162a2bc5dd8a8acb8fdfe623f120
Ryo Suzuki <ryo.s12330@gmail.com>
2018-12-09 02:09:33 +0900

    commit

--- index.js
+++ index.js
@@ -8,59 +8,52 @@ const line_config = {
 
 server.listen(process.env.PORT || 3000);
 
-console.log('gegegegege');
-
-server.post('/webhook', line.middleware(line_config), (req, res, next) => {
-    res.sendStatus(200);
-    console.log('ここで出ないのはおかしい')
-    console.log(req.body);
-});
+// server.post('/webhook', line.middleware(line_config), (req, res, next) => {
+//     res.sendStatus(200);
+//     console.log('ここで出ないのはおかしい');
+//     console.log(req.body);
+// });
 
 const bot = new line.Client(line_config);
 
-server.post('/webhook', function (req, res) {
-    res.sendStatus(200);
-  console.log('gege');
-    let events_processed = [];
-
-    // events_processed.push(bot.replyMessage(event.replyToken, {
-    //                 type: "text",
-    //                 text: "これはこれは"
-    //             }));
-});
-
-// server.post('/webhook', line.middleware(line_config), (req, res, next) => {
-//     // 先行してLINE側にステータスコード200でレスポンスする。
+// server.post('/webhook', function (req, res) {
 //     res.sendStatus(200);
-//     console.log('hoge');
-
-//     // すべてのイベント処理のプロミスを格納する配列。
+//   console.log('gege');
 //     let events_processed = [];
 
-//     events_processed.push(bot.replyMessage(event.replyToken, {
-//                     type: "text",
-//                     text: "これはこれは"
-//                 }));
-
-//     // // イベントオブジェクトを順次処理。
-//     // req.body.events.forEach((event) => {
-//     //     // この処理の対象をイベントタイプがメッセージで、かつ、テキストタイプだった場合に限定。
-//     //     if (event.type == "message" && event.message.type == "text"){
-//     //         // ユーザーからのテキストメッセージが「こんにちは」だった場合のみ反応。
-//     //         if (event.message.text == "よろしく"){
-//     //             // replyMessage()で返信し、そのプロミスをevents_processedに追加。
-//     //             events_processed.push(bot.replyMessage(event.replyToken, {
+//     // events_processed.push(bot.replyMessage(event.replyToken, {
 //     //                 type: "text",
 //     //                 text: "これはこれは"
 //     //             }));
-//     //         }
-//     //     }
-//     // });
-
-//     // すべてのイベント処理が終了したら何個のイベントが処理されたか出力。
-//     Promise.all(events_processed).then(
-//         (response) => {
-//             console.log(`${response.length} event(s) processed.`);
-//         }
-//     );
 // });
+
+server.post('/webhook', line.middleware(line_config), (req, res, next) => {
+    // 先行してLINE側にステータスコード200でレスポンスする。
+    res.sendStatus(200);
+    console.log('ここで出ないのはおかしい');
+
+    // すべてのイベント処理のプロミスを格納する配列。
+    let events_processed = [];
+
+    // イベントオブジェクトを順次処理。
+    req.body.events.forEach((event) => {
+        // この処理の対象をイベントタイプがメッセージで、かつ、テキストタイプだった場合に限定。
+        if (event.type == "message" && event.message.type == "text"){
+            // ユーザーからのテキストメッセージが「こんにちは」だった場合のみ反応。
+            if (event.message.text == "よろしく"){
+                // replyMessage()で返信し、そのプロミスをevents_processedに追加。
+                events_processed.push(bot.replyMessage(event.replyToken, {
+                    type: "text",
+                    text: "これはこれは"
+                }));
+            }
+        }
+    });
+
+    // すべてのイベント処理が終了したら何個のイベントが処理されたか出力。
+    // Promise.all(events_processed).then(
+    //     (response) => {
+    //         console.log(`${response.length} event(s) processed.`);
+    //     }
+    // );
+});

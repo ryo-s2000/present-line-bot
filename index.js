d6dd72f207b3a80554dfdde6991466edde6df00b
Ryo Suzuki <ryo.s12330@gmail.com>
2018-12-09 01:16:43 +0900

    second

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
 const server = require("express")();
 const line = require("@line/bot-sdk"); // Messaging APIのSDKをインポート
 
-// // -----------------------------------------------------------------------------
-// // パラメータ設定
-// const line_config = {
-//     channelAccessToken: process.env.LINE_ACCESS_TOKEN, // 環境変数からアクセストークンをセットしています
-//     channelSecret: process.env.LINE_CHANNEL_SECRET // 環境変数からChannel Secretをセットしています
-// };
+// -----------------------------------------------------------------------------
+// パラメータ設定
+const line_config = {
+    channelAccessToken: process.env.LINE_ACCESS_TOKEN, // 環境変数からアクセストークンをセットしています
+    channelSecret: process.env.LINE_CHANNEL_SECRET // 環境変数からChannel Secretをセットしています
+};
 
-// // -----------------------------------------------------------------------------
-// // Webサーバー設定
-// server.listen(process.env.PORT || 3000);
+// -----------------------------------------------------------------------------
+// Webサーバー設定
+server.listen(process.env.PORT || 3000);
 
-// // -----------------------------------------------------------------------------
-// // ルーター設定
-// server.post('/webhook', line.middleware(line_config), (req, res, next) => {
-//     res.sendStatus(200);
-//     console.log(req.body);
-// });
+// -----------------------------------------------------------------------------
+// ルーター設定
+server.post('/webhook', line.middleware(line_config), (req, res, next) => {
+    res.sendStatus(200);
+    console.log(req.body);
+});
 
 // const bot = new line.Client(line_config);
 

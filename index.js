dcb0e346f0ed7a77c455a19b3a6b10e2bc323d30
Ryo Suzuki <ryo.s12330@gmail.com>
2018-12-09 02:13:05 +0900

    commit

--- index.js
+++ index.js
@@ -1,47 +1,24 @@
 const server = require("express")();
-const line = require("@line/bot-sdk"); // Messaging APIのSDKをインポート
+const line = require("@line/bot-sdk");
 
 const line_config = {
-    channelAccessToken: process.env.LINE_ACCESS_TOKEN, // 環境変数からアクセストークンをセットしています
-    channelSecret: process.env.LINE_CHANNEL_SECRET // 環境変数からChannel Secretをセットしています
+    channelAccessToken: process.env.LINE_ACCESS_TOKEN,
+    channelSecret: process.env.LINE_CHANNEL_SECRET
 };
 
 server.listen(process.env.PORT || 3000);
 
-// server.post('/webhook', line.middleware(line_config), (req, res, next) => {
-//     res.sendStatus(200);
-//     console.log('ここで出ないのはおかしい');
-//     console.log(req.body);
-// });
-
 const bot = new line.Client(line_config);
 
-// server.post('/webhook', function (req, res) {
-//     res.sendStatus(200);
-//   console.log('gege');
-//     let events_processed = [];
-
-//     // events_processed.push(bot.replyMessage(event.replyToken, {
-//     //                 type: "text",
-//     //                 text: "これはこれは"
-//     //             }));
-// });
-
 server.post('/webhook', line.middleware(line_config), (req, res, next) => {
-    // 先行してLINE側にステータスコード200でレスポンスする。
+    
     res.sendStatus(200);
-    console.log('ここで出ないのはおかしい');
 
-    // すべてのイベント処理のプロミスを格納する配列。
     let events_processed = [];
 
-    // イベントオブジェクトを順次処理。
     req.body.events.forEach((event) => {
-        // この処理の対象をイベントタイプがメッセージで、かつ、テキストタイプだった場合に限定。
         if (event.type == "message" && event.message.type == "text"){
-            // ユーザーからのテキストメッセージが「こんにちは」だった場合のみ反応。
             if (event.message.text == "よろしく"){
-                // replyMessage()で返信し、そのプロミスをevents_processedに追加。
                 events_processed.push(bot.replyMessage(event.replyToken, {
                     type: "text",
                     text: "これはこれは"
@@ -50,10 +27,10 @@ server.post('/webhook', line.middleware(line_config), (req, res, next) => {
         }
     });
 
-    // すべてのイベント処理が終了したら何個のイベントが処理されたか出力。
-    // Promise.all(events_processed).then(
-    //     (response) => {
-    //         console.log(`${response.length} event(s) processed.`);
-    //     }
-    // );
+    Promise.all(events_processed).then(
+        (response) => {
+            console.log(`${response.length} event(s) processed.`);
+        }
+    );
+
 });

1ea6a7d0a959f916d7a280435106baa37f344d10
Ryo Suzuki <ryo.s12330@gmail.com>
2018-12-09 01:56:50 +0900

    commit

--- index.js
+++ index.js
@@ -15,9 +15,9 @@ server.listen(process.env.PORT || 3000);
 // -----------------------------------------------------------------------------
 // ルーター設定
 server.post('/webhook', line.middleware(line_config), (req, res, next) => {
-    // res.sendStatus(200);
-    // console.log('hoho');
-    // console.log(req.body);
+    res.sendStatus(200);
+    console.log('hoho');
+    console.log(req.body);
 });
 
 // const bot = new line.Client(line_config);

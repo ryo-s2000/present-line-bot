173e1ef39721c1523f1774ef17e4051606ffb189
Ryo Suzuki <ryo.s12330@gmail.com>
2018-12-09 01:44:01 +0900

    gege

--- index.js
+++ index.js
@@ -21,47 +21,47 @@ server.post('/webhook', line.middleware(line_config), (req, res, next) => {
 
 const bot = new line.Client(line_config);
 
-console.log('hogehoge');
+console.log('hoho');
 
 // -----------------------------------------------------------------------------
 // ルーター設定
 
 server.post('/webhook', function (req, res) {
-  console.log('hoge');
+  console.log('gege');
 });
 
-server.post('/webhook', line.middleware(line_config), (req, res, next) => {
-    // 先行してLINE側にステータスコード200でレスポンスする。
-    res.sendStatus(200);
-    console.log('hoge');
+// server.post('/webhook', line.middleware(line_config), (req, res, next) => {
+//     // 先行してLINE側にステータスコード200でレスポンスする。
+//     res.sendStatus(200);
+//     console.log('hoge');
 
-    // すべてのイベント処理のプロミスを格納する配列。
-    let events_processed = [];
+//     // すべてのイベント処理のプロミスを格納する配列。
+//     let events_processed = [];
 
-    events_processed.push(bot.replyMessage(event.replyToken, {
-                    type: "text",
-                    text: "これはこれは"
-                }));
+//     events_processed.push(bot.replyMessage(event.replyToken, {
+//                     type: "text",
+//                     text: "これはこれは"
+//                 }));
 
-    // // イベントオブジェクトを順次処理。
-    // req.body.events.forEach((event) => {
-    //     // この処理の対象をイベントタイプがメッセージで、かつ、テキストタイプだった場合に限定。
-    //     if (event.type == "message" && event.message.type == "text"){
-    //         // ユーザーからのテキストメッセージが「こんにちは」だった場合のみ反応。
-    //         if (event.message.text == "よろしく"){
-    //             // replyMessage()で返信し、そのプロミスをevents_processedに追加。
-    //             events_processed.push(bot.replyMessage(event.replyToken, {
-    //                 type: "text",
-    //                 text: "これはこれは"
-    //             }));
-    //         }
-    //     }
-    // });
+//     // // イベントオブジェクトを順次処理。
+//     // req.body.events.forEach((event) => {
+//     //     // この処理の対象をイベントタイプがメッセージで、かつ、テキストタイプだった場合に限定。
+//     //     if (event.type == "message" && event.message.type == "text"){
+//     //         // ユーザーからのテキストメッセージが「こんにちは」だった場合のみ反応。
+//     //         if (event.message.text == "よろしく"){
+//     //             // replyMessage()で返信し、そのプロミスをevents_processedに追加。
+//     //             events_processed.push(bot.replyMessage(event.replyToken, {
+//     //                 type: "text",
+//     //                 text: "これはこれは"
+//     //             }));
+//     //         }
+//     //     }
+//     // });
 
-    // すべてのイベント処理が終了したら何個のイベントが処理されたか出力。
-    Promise.all(events_processed).then(
-        (response) => {
-            console.log(`${response.length} event(s) processed.`);
-        }
-    );
-});
+//     // すべてのイベント処理が終了したら何個のイベントが処理されたか出力。
+//     Promise.all(events_processed).then(
+//         (response) => {
+//             console.log(`${response.length} event(s) processed.`);
+//         }
+//     );
+// });
